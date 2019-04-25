6752add77d95a62e5053ebb839d5cef0505a24b0
yelingxue <44341061+yelingxue@users.noreply.github.com>
2019-04-25 20:28:40 +0800

    Create primer.cpp

---
+++ primer.cpp
@@ -0,0 +1,38 @@
+#include<stdio.h>
+
+void isPrimer(int n);
+
+int main(void)
+{
+	int n;
+	scanf("%d",&n);
+
+	isPrimer(n);
+	return 0;
+}
+
+void isPrimer(int n)
+{
+	int line = 0;
+	int a[2000];
+	for (int i = 0; i < n+1; i++) 
+	{
+		a[i] = 1;	
+	}
+	for (int j = 2; j < n+1; j++)
+	{
+		if(1 == a[j])
+		{
+			for (int k = 1; k*j < n+1; k++)
+			{
+				a[j*k] = 0;
+			}
+			line++;
+			printf("%d    ",j);
+			if(0 == line%5)
+			{
+				printf("\n");
+			}
+		}
+	}
+}
